950115b72ac61a7703084eae3db84c303b39f687
luke08082000 <99248414+luke08082000@users.noreply.github.com>
2022-11-27 22:04:31 +0800

    async nov 26

--- cloud_app.js
+++ cloud_app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const Painting = require('./painting')
+const crypto = require('crypto');
 const app = express()
 
 const dbUri = "mongodb+srv://luke:Q5Tnaog8B86vOWez@cluster0.nydmrhf.mongodb.net/?retryWrites=true&w=majority";
@@ -59,3 +60,38 @@ app.delete('/delete/:id', (req, res) => {
             res.send("cannot delete...")
         })
 })
+
+const artistName = "Caesar Legaspi";
+const key = 'password123';
+
+const cipher = crypto.createCipher('aes192', key);
+var encrypted = cipher.update(artistName, 'utf8', 'hex');
+encrypted += cipher.final('hex');
+
+const doc1 = {title: "Marbling Universe", artist: encrypted, dimensions: "424x540", date_created: "3/11/2015", paint: "Oil"};
+
+
+app.post('/encrypt', (req, res) => {
+    const p2 = new Painting(doc1)
+    p2.save()
+        .then(result => res.send(result))
+        .catch(err => console.log(err))
+})
+
+//decrypt
+app.get('/decrypt/:id',(req, res) => {
+    Painting.findById(req.params.id)
+    .then(result => {
+        let artist = result.artist;
+        const decipher = crypto.createDecipher('aes192', key);
+        var decrypted = decipher.update(artist, 'hex', 'utf8');
+        decrypted += decipher.final('utf8');
+        res.send(decrypted);
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
+
+
+
--- painting.js
+++ painting.js
@@ -1,5 +1,7 @@
 const { Timestampt } = require('mongodb')
 const mongoose = require('mongoose')
+const crypto = require('crypto');
+const { appendFile } = require('fs');
 
 const paintingSchema = mongoose.Schema({
     title: {
@@ -23,6 +25,10 @@ const paintingSchema = mongoose.Schema({
         required: true
     }
 }, {versionKey: false}, {timestamps: false})
+
+paintingSchema.pre('save', function() {
+    this.paint = crypto.createHash('sha256').update(this.paint).digest('hex');
+})
 
 const Painting = mongoose.model('painting', paintingSchema, 'painting')
 module.exports = Painting
